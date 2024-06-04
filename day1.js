b5474543148d329da391a87dae5d560699b806d0
Malikibdah <malikibdah88@gmail.com>
2024-06-04 16:16:10 +0300

    4/6/2024

---
+++ day1.js
@@ -0,0 +1,129 @@
+/*
+
+let x = 250 ;
+create variable Calculate the value of zakat for x ,If you know the percentage of zakat = 2.5 %; 
+
+*/
+// solution
+// let x=250;
+// let y=0.025;
+// let z=x*y;
+// console.log(z);
+
+/*
+Array Tasks : 
+1
+Correct the syntax error
+ [ 1,7  9  45, ]
+
+ ["Str" "alex","moh"
+
+ 'the','fox' 'over' lazy, 'dog',  ]
+
+*/
+// solution
+// [ 1,7 , 9 , 45 ]
+
+// ["Str", "alex","moh" ]
+// ['the','fox' ,'over', 'lazy', 'dog'] 
+
+/*
+2
+What the index of "Banana","Tomato"
+var fruits=["Tomato","Banana","Watermelon"]
+
+*/
+// solution
+// banana=[1];
+// Tomato=[0];
+
+/*
+3
+Create an array represents your:
+1- Favorite Food (2)
+2- Favorite Sport (3)
+3- Favorite Movie (2)
+*/
+// solution
+// const Malik = [Shawerma , Mansaf , Swimming , Football , Tenes , Johnwick , jason]
+
+/*
+4
+Create a Variable to return the first element in an array 
+Ex: firstOfArray([1,4,5]) => 1
+Ex: firstOfArray(["t","u","g","x"]) => "t"
+*/
+// solution
+// const firstelement = [1 , 2 , 34 ,45];
+// console.log(firstelement[0]);
+
+/*
+5
+Create a Variable to return the lastOfArray element in an array 
+
+Ex: lastOfArray([1,4,5]) => 5
+Ex: lastOfArray(["t","u","g","x"]) => "x"
+*/
+// solution
+// const lastarray = [ 1, 2 ,3, 5 ,55,78];
+// const x= lastarray.length -1;
+// console.log(lastarray[x]);
+
+/*
+6
+Using console make this array to be like this one (push, unshift, shift, pop)
+
+var array = [0,5,7,9]
+=> [1,3,4,6,8,9,10]
+*/
+// solution1
+// const array = [0,5,7,9];
+// array.splice(0 ,3 , 1 , 3 , 4 , 6 , 8); //splice(start index remove , number item delet, numbers to add between them a, b,..)
+// array.push(10);
+// solution2
+// const array = [0,5,7,9];
+// array.push(10);
+// array.shift(); //remove first index
+// array.shift();
+// array.shift();
+// array.unshift(8); //add new index in the first
+// array.unshift(6);
+// array.unshift(4);
+// array.unshift(3);
+// array.unshift(1);
+
+// console.log(array);
+
+/*
+7
+Using the console try to figure out what the thing thats (push, unshift, shift, pop) return to you
+
+var array2 = [5,9,-7,3.5]
+*/
+// solution
+// var array = [0,5,7,9];
+// array.shift();
+// array.shift();
+// array.shift();
+// array.unshift(5);
+// array.push(-7);
+// array.push(3.5);
+
+// console.log(array);
+
+
+/*
+8.
+Write a JavaScript program to sort the items of an array.
+Sample array : var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+Sample Output : -4,-3,1,2,3,5,6,7,8
+*/
+// solution
+
+// var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+// function malik (a,b) {
+//     return (a-b)
+// };
+// arr1.sort(malik);
+// console.log(arr1);
+
