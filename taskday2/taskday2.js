65bf189bcbb5d58e0b19c0db222e19d31bc978b2
Malikibdah <malikibdah88@gmail.com>
2024-06-05 00:00:49 +0300

    5/6/2024

---
+++ taskday2/taskday2.js
@@ -0,0 +1,165 @@
+/* 1. Write a JavaScript program that accept two integers and
+ * display the larger.Go to the editor */
+/******* Start Your Code *********/
+// solution
+// let x=5;
+// let y=8;
+// if (x>y){
+//     console.log(x)
+// }else{
+//     console.log(y)
+// };
+
+/* 2. Write a JavaScript conditional statement to find the sign
+ of product of three numbers. Display an alert box with the 
+ specified sign. Go to the editor 
+Sample numbers : 3, -7, 2 
+Output : The sign is - */
+/******* Start Your Code *********/
+
+// solution
+// let condstat= [4,-7,9];
+// for(i of condstat){
+//     if(i>=0){
+//         window.alert("is postive")
+//     } else{
+//         window.alert("is negative")
+//     }
+// }
+/* 3. Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result. Go to the editor 
+Sample numbers : 0, -1, 4 
+Output : 4, 0, -1 */
+/******* Start Your Code *********/
+// solution
+// let x= 222;
+// let m= 566;
+// let h= 59;
+
+// if(x>m && x>h){
+//     if(m>h){
+//         window.alert('x' + 'm' + 'h')
+//     }else{
+//         window.alert('x' + 'h' + 'm')
+//     }
+// }
+
+// else if (m>h && m>x){
+//     if(h>x){
+//         window.alert('m' + 'h' + 'x')
+//     }else{
+//         window.alert('m' + 'x' + 'h')
+//     }
+
+// }
+// else if (h>x && h>m){
+//     if(x>m){
+//         window.alert('h' + 'x' + 'm')
+//     }else {
+//         window.alert('h' + 'm' + 'x')
+//     }
+// }
+
+/*4. Write a JavaScript conditional statement to find the largest of five numbers. 
+Display an alert box to show the result. Go to the editor 
+Sample numbers : -5, -2, -6, 0, -1 
+Output : 0 */
+// solution
+// let x=2;
+// let y=4;
+// let z=9;
+// let m=5;
+// let g=12;
+// if(x>y && x>z && x>m && x>g){
+//     window.alert("The larg nu. is"+"  "+ "x=" +x)
+// }
+
+// else if (y>x && y>z && y>m && y>g){
+//     window.alert("The larg nu. is"+"  "+ "y=" +y)
+// }
+// else if (z>x && z>y && z>m && z>g){
+//     window.alert("The larg nu. is"+"  "+ "z=" +z)
+// }
+// else if (m>x && m>y && m>z && m>g){
+//     window.alert("The larg nu. is"+"  "+ "m=" +m)
+// }
+// else if (g>x && g>y && g>m && g>z){
+//     window.alert("The larg nu. is"+"  "+"g=" + g)
+// }
+/* 5.Write the if statement to display "Hello World" if x is greater than y, otherwise alert "Goodbye".*/
+// solution
+// let x=6;
+// let y=54;
+// if (x>y){
+//     window.alert("Hello World")
+// }else{
+//     window.alert("Goodbye")
+// }
+/* 1. Write a JavaScript program that returns rate as text
+less than 50 return ==> Fail
+equal or between 50 and 59 ===> E
+equal or between 60 and 69 ===> D
+equal or between 70 and 79 ===> C
+equal or between 80 and 89 ===> B
+equal or between 90 and 100 ===> A
+*/
+// solution
+// function exam(m){
+//     if (m<50){
+//         return "Fail";
+//     }
+//     else if(m>=50 && m<=59){
+//         return "E";}
+//     else if(m>=60 && m<=69){
+//         return "D";}
+//     else if(m>=70 && m<=79){
+//         return "C";}
+//     else if(m>=80 && m<=89){
+//         return "B";}
+//     else if(m>=90 && m<=100){
+//         return "A";}
+//     };
+//     console.log(exam(88));
+
+// 1. Write a JS code to print numbers from 1 to 10
+// solution
+// 
+
+/*2. Write a JS code to print Even numbers in arr :
+   var arr = [13,23,12,45,22,48,66,100]
+*/
+// solution
+// var arr = [13,23,12,45,22,48,66,100];
+// let even=[];
+// for(let i=0 ; i < arr.length ; i++){
+//     if(arr[i]%2==0){
+//         even.push(arr[i]);
+//     }
+// }
+// console.log(even);
+
+/* 
+3. Write a JS code to print a pattern using for loop
+
+   1 
+   1 2 
+   1 2 3 
+   1 2 3 4 
+   1 2 3 4 5 
+   1 2 3 4 5 6 
+   1 2 3 4 5 6 7 
+   1 2 3 4 5 6 7 8 
+
+*/
+// solution
+// for(let i=1 ;i<=8; i++ ){
+//     for(let m=1 ; m<=i ; m++){
+//         console.log(m);
+//     }
+// }
+
+/* 
+ Check if a string contains the letter “y”. Print “yes” if it does and “no” if it does not.
+let x = "don’t know why"
+
+*/
+// solution
